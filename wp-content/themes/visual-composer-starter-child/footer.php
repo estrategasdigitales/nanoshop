dd6606b5e0b39f8b9b744ff6458f9d5e07d56d4f
Adrian Guerrero <guerreroradrian1111@gmail.com>
2020-04-24 08:45:58 -0500

    subiendo inicial

---
+++ wp-content/themes/visual-composer-starter-child/footer.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Footer
+ *
+ * @package WordPress
+ * @subpackage Visual Composer Starter
+ * @since Visual Composer Starter 1.0
+ */
+
+if ( visualcomposerstarter_is_the_footer_displayed() ) : ?>
+	<?php visualcomposerstarter_hook_before_footer(); ?>
+	<footer id="footer">	
+		<div class="row">
+			<div class="container">
+				<div class="col-md-4 text-center redesf">
+					<a href="<?php echo site_url(); ?>/aviso-de-privacidad">Aviso de privacidad</a>
+				</div>
+				<div class="col-md-4 text-center redesf">
+					
+					
+					Sitio desarrollado por <a href="">Estrategas Digitales</a>
+					
+					
+				</div>
+				
+				<div class="col-md-4 text-center redesf">
+					<span>2020. Solho. Todos los derechos reservados</span>
+						
+				</div>
+				
+			</div>
+		</div>
+		
+	</footer>
+
+
+
+
+
+
+<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
+
+<script type="text/javascript">
+	jQuery(document).ready(function ($) {
+
+ 
+ var $container = $('#isotope-list'); //The ID for the list with all the blog posts
+ $container.isotope({ //Isotope options, 'item' matches the class in the PHP
+ filter: '*',
+ itemSelector : '.item', 
+   layoutMode : 'masonry'
+ });
+ 
+ //Add the class selected to the item that is clicked, and remove from the others
+ var $optionSets = $('#filters'),
+ $optionLinks = $optionSets.find('a');
+ 
+ $optionLinks.click(function(){
+ var $this = $(this);
+ // don't proceed if already selected
+ if ( $this.hasClass('selected') ) {
+   return false;
+ }
+ var $optionSet = $this.parents('#filters');
+ $optionSets.find('.selected').removeClass('selected');
+ $this.addClass('selected');
+ 
+ //When an item is clicked, sort the items.
+ var selector = $(this).attr('data-filter');
+ $container.isotope({ filter: selector });
+ 
+ return false;
+ });
+ 
+});
+</script>
+
+
+	<?php visualcomposerstarter_hook_after_footer(); ?>
+<?php endif; ?>
+<?php wp_footer(); ?>
+
+</body>
+</html>
+
+
+
+
